5870961de2fcbc3c089f3026f12fde23d75d31b2
slahiri <slahiri@visteon.com>
2026-04-11 13:52:56 +0530

    Revert "first push"
    This reverts commit f1bd32831c06718598de18dec50dcb4c421244e0.

--- lib/perf.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file perf.c
- * @brief Performance measurement global state.
- */
-
-#include "perf.h"
-
-/* Global I/O suppression flag (0 = normal output, 1 = suppress diagnostics) */
-volatile int g_suppress_io = 0;
